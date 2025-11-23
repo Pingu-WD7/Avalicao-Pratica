f80b1251c454db30a51551e396ff21fa6beeee66
Pingu <felipe_guaresi@estudante.sesisenai.org.br>
2025-11-22 23:15:54 -0300

    Add files via upload

---
+++ AvaliaçãoPratica/js/script.js
@@ -0,0 +1,176 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const agentesLink = document.querySelector('a[href="/html/cubo.html"]');
+    if (agentesLink) {
+        agentesLink.addEventListener('click', function() {
+            alert('Você está sendo direcionado para a seção do cubo magico!');
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const agentesLink = document.querySelector('a[href="/html/jogo.html"]');
+    if (agentesLink) {
+        agentesLink.addEventListener('click', function() {
+            alert('Você está sendo direcionado para a seção do jogos!');
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const agentesLink = document.querySelector('a[href="/html/edicao.html"]');
+    if (agentesLink) {
+        agentesLink.addEventListener('click', function() {
+            alert('Você está sendo direcionado para a seção da edicao!');
+        });
+    }
+});
+
+const imagem1 = document.getElementById('cuboimg');
+if (imagem1) {
+  imagem1.addEventListener('mouseenter', () => {
+    imagem1.style.transform = 'scale(1.2)';
+  });
+  imagem1.addEventListener('mouseleave', () => {
+    imagem1.style.transform = 'scale(1)';
+  });
+}
+
+const imagem2 = document.getElementById('adobeimg1');
+if (imagem2) {
+  imagem2.addEventListener('mouseenter', () => {
+    imagem2.style.transform = 'scale(1.2)';
+  });
+  imagem2.addEventListener('mouseleave', () => {
+    imagem2.style.transform = 'scale(1)';
+  });
+}
+
+const imagem3 = document.getElementById('jogoimg');
+if (imagem3) {
+  imagem3.addEventListener('mouseenter', () => {
+    imagem3.style.transform = 'scale(1.2)';
+  });
+  imagem3.addEventListener('mouseleave', () => {
+    imagem3.style.transform = 'scale(1)';
+  });
+}
+
+const imagem4 = document.getElementById('img4');
+if (imagem4) {
+  imagem4.addEventListener('mouseenter', () => {
+    imagem4.style.transform = 'scale(1.2)';
+  });
+  imagem4.addEventListener('mouseleave', () => {
+    imagem4.style.transform = 'scale(1)';
+  });
+}
+
+const imagem5 = document.getElementById('img5');
+if (imagem5) {
+  imagem5.addEventListener('mouseenter', () => {
+    imagem5.style.transform = 'scale(1.2)';
+  });
+  imagem5.addEventListener('mouseleave', () => {
+    imagem5.style.transform = 'scale(1)';
+  });
+}
+
+const imagem6 = document.getElementById('img6');
+if (imagem6) {
+  imagem6.addEventListener('mouseenter', () => {
+    imagem6.style.transform = 'scale(1.2)';
+  });
+  imagem6.addEventListener('mouseleave', () => {
+    imagem6.style.transform = 'scale(1)';
+  });
+}
+
+const imagem7 = document.getElementById('img7');
+if (imagem7) {
+  imagem7.addEventListener('mouseenter', () => {
+    imagem7.style.transform = 'scale(1.2)';
+  });
+  imagem7.addEventListener('mouseleave', () => {
+    imagem7.style.transform = 'scale(1)';
+  });
+}
+
+const imagem8 = document.getElementById('img8');
+if (imagem8) {
+  imagem8.addEventListener('mouseenter', () => {
+    imagem8.style.transform = 'scale(1.2)';
+  });
+  imagem8.addEventListener('mouseleave', () => {
+    imagem8.style.transform = 'scale(1)';
+  });
+}
+
+const imagem9 = document.getElementById('img9');
+if (imagem9) {
+  imagem9.addEventListener('mouseenter', () => {
+    imagem9.style.transform = 'scale(1.2)';
+  });
+  imagem9.addEventListener('mouseleave', () => {
+    imagem9.style.transform = 'scale(1)';
+  });
+}
+
+const imagem10 = document.getElementById('img10');
+if (imagem10) {
+  imagem10.addEventListener('mouseenter', () => {
+    imagem10.style.transform = 'scale(1.2)';
+  });
+  imagem10.addEventListener('mouseleave', () => {
+    imagem10.style.transform = 'scale(1)';
+  });
+}
+
+const imagem11 = document.getElementById('img11');
+if (imagem11) {
+  imagem11.addEventListener('mouseenter', () => {
+    imagem11.style.transform = 'scale(1.2)';
+  });
+  imagem11.addEventListener('mouseleave', () => {
+    imagem11.style.transform = 'scale(1)';
+  });
+}
+
+const imagem12 = document.getElementById('img12');
+if (imagem12) {
+  imagem12.addEventListener('mouseenter', () => {
+    imagem12.style.transform = 'scale(1.2)';
+  });
+  imagem12.addEventListener('mouseleave', () => {
+    imagem12.style.transform = 'scale(1)';
+  });
+}
+
+const imagem13 = document.getElementById('img13');
+if (imagem13) {
+  imagem13.addEventListener('mouseenter', () => {
+    imagem13.style.transform = 'scale(1.2)';
+  });
+  imagem13.addEventListener('mouseleave', () => {
+    imagem13.style.transform = 'scale(1)';
+  });
+}
+
+const imagem14 = document.getElementById('img14');
+if (imagem14) {
+  imagem14.addEventListener('mouseenter', () => {
+    imagem14.style.transform = 'scale(1.2)';
+  });
+  imagem14.addEventListener('mouseleave', () => {
+    imagem14.style.transform = 'scale(1)';
+  });
+}
+
+const imagem15 = document.getElementById('img15');
+if (imagem15) {
+  imagem15.addEventListener('mouseenter', () => {
+    imagem15.style.transform = 'scale(1.2)';
+  });
+  imagem15.addEventListener('mouseleave', () => {
+    imagem15.style.transform = 'scale(1)';
+  });
+}
